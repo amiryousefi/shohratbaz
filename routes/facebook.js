198c22715e92d98c5382bc0147a680c51dc286ad
Amir Yousefi <amiryousefi.it@gmail.com>
2019-07-24 11:53:21 +0430

    Add facebook groups route to facebook router

--- routes/facebook.js
+++ routes/facebook.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 const facebook_controller = require('../controllers/facebook.controller');
 
+// auth facebook
 router.get('/auth', facebook_controller.auth);
+
+// get facebook callback response
 router.get('/callback', facebook_controller.callback);
 
 module.exports = router;
